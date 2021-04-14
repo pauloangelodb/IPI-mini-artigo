e3b10b0b64049e122a6f3d58d1c993ff94ec96bd
leticiafraga <leticiafraga55@gmail.com>
2021-04-14 20:12:03 -0300

    Update getDays.py

--- getDays.py
+++ getDays.py
@@ -1,32 +1,51 @@
 import json
 
 def checkAuthorCommitsDays(dados_api, authorMap, fileLogRaw):
-	for i in range(len(dados_api)):
 		date = dados_api[i]['commit']['author']['date']
 		#author = dados_api[i]['commit']['author']['email']
-		d = date +"\n"
-		fileLogRaw.write(d)
-
-		day = date.split("T")[0]
+    for i in range(len(dados_api)):
+        
+        #author = i['commit']['author']['email']
+        #print(author)
+        #está reconhecendo dados_api[i] como string
+        commit =  dados_api[i].get('commit')
+        author = commit.get('author')
+        date = author.get('date')
+            
+            
+        day = date.split("T")[0]
         time = date.split("T")[1]
+        hour = time.split(":")[0]
+            
+        #print(day +"\n"+hour+"\n\n")
 
-		if not author in authorMap:
-			authorMap[author] = [day]
-		else:
-			if not day in authorMap[author]:
-				authorMap[author].append(day)
-	
+        d = day + "," + hour + "\n"
+        fileLogRaw.write(d)
+
+        '''if not author in authorMap: #lista com os autores e lista com os dias
+            authorMap[author] = [day]
+        else:
+            if not day in authorMap[author]:
+                authorMap[author].append(day)
+            else:
+                authorMap[author][day].append(hour)'''
+        if not day in authorMap:
+            authorMap[day] = [hour]
+        else:
+            authorMap[day].append(hour)
+        
+
 def computeAuthorRanks(fileActivity, authorMap):
-	
-	numCommits = []
-	for key, value in authorMap.items():
-		print(key, len(value))
-		numCommits.append(len(value))
+    
+    numCommits = []
+    for key, value in authorMap.items():
+        print(key, len(value))
+        numCommits.append(len(value))
 
-	i = 1
-	for value in sorted(numCommits,reverse=True):
-		fileActivity.write(str(i)+","+str(value)+"\n")
-		i += 1
+    i = 1
+    for value in sorted(numCommits,reverse=True):
+        fileActivity.write(str(i)+","+str(value)+"\n")
+        i += 1
 
 
 fileJSON = open('data.json', 'r', encoding='utf-8') 
@@ -39,4 +58,4 @@ fileLogRaw.close()
 fileActivity = open("activity.data", "w")
 fileActivity.write("Rank,Frequency\n")
 computeAuthorRanks(fileActivity, authorMap)
-fileActivity.close
+fileActivity.close()
