9b56b23be1fa38dbc751adc27cb146ed02f0924d
leticiafraga <74565664+leticiafraga@users.noreply.github.com>
2021-04-17 17:19:44 -0300

    Update graphActivitySemanas.R
    Atualização da formatação do gráfico

--- graphActivitySemanas.R
+++ graphActivitySemanas.R
@@ -7,16 +7,11 @@ Dias<-c("Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado","Domingo")
 df_activity
 
 pl <- ggplot(df_activity,aes(x = Dia, y=Frequencia)) +
-ggtitle("Commits por dia da semana")+
-
-theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
-geom_line(color ="blue", lwd=1.5)+
-scale_x_continuous(breaks=(0:6),labels= c("S", "T", "Q", "Q", "S", "S", "D"))
-
+  ggtitle("Commits em função do dia da semana")+
+  
+  theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
+  geom_line(lwd=1.5) +
+  scale_x_continuous(breaks=(0:6),labels= Dias) +
+  scale_y_continuous(limits = c(0, 1200))
 
 print(pl)
-
-#plot(df_activity, xaxt="n",main="Quantidade de commits em função do dia da semana", type="l")
-#axis(1, at=0:6, labels=Dias, font=2)
-#axis(2, seq(0,1200,200), font=2)
-
