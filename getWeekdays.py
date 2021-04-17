95ed8c79bd9343931f5eebe59a5ffcb4108ca7b6
leticiafraga <74565664+leticiafraga@users.noreply.github.com>
2021-04-17 14:55:51 -0300

    Update getWeekdays.py

--- getWeekdays.py
+++ getWeekdays.py
@@ -1,7 +1,6 @@
-
 import datetime
-
 import json
+
 def checkWeekDays(dados_api, weekMap, fileLogRaw, fileActivity):
     
     data =[]
@@ -11,40 +10,26 @@ def checkWeekDays(dados_api, weekMap, fileLogRaw, fileActivity):
         
         if type(i)==dict:
             
-            commit =  i['commit']
-            author = commit.get('author')
-            date2 = author.get('date')
-               
-            #date = dados_api[i]['commit']['author']['date']
+            date2 =  i['commit']['author']['date']
+            #formato: "2021-04-14T17:55:56Z"
+            
             date1 = date2.split("T")[0]
             year = date1.split("-")[0]
             if year =="2021":
                 month = date1.split("-")[1]
                 day = date1.split("-")[2]
-
-                #"2021-04-14T17:55:56Z"
-
                 data.append([year,month,day])
     
     for commit in sorted(data,reverse=True):
         
-        
-        
         wd = datetime.date(int(commit[0]),int(commit[1]),int(commit[2])).weekday()
         #print(commit[0],commit[1],commit[2], wd)
         fileLogRaw.write(commit[0]+"-"+commit[1]+"-"+commit[2]+ " "+str(wd)+"\n")
-        
-        
         contaWD[wd] += 1
-        
-    print(contaWD)
-            
+    
     for i in range(len(contaWD)):
         fileActivity.write(str(i)+","+str(contaWD[i])+"\n")
                 
-        
-    
-        
 
 fileJSON = open('data.json', 'r', encoding='utf-8') 
 dados_api = json.load(fileJSON)
@@ -54,4 +39,4 @@ fileActivity = open("activityWD.data", "w")
 fileActivity.write("Dia,Frequencia\n")
 checkWeekDays(dados_api, weekMap,fileLogRaw, fileActivity)
 fileLogRaw.close()
-
+fileActivity.close()
