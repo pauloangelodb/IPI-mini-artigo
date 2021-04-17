f885dbe15e6dea394e3cc906cc1a34fe4c641b76
leticiafraga <74565664+leticiafraga@users.noreply.github.com>
2021-04-17 17:14:09 -0300

    Update graphActivityHoras.R

--- graphActivityHoras.R
+++ graphActivityHoras.R
@@ -1,4 +1,3 @@
-install.packages("ggplot2")
 library(ggplot2)
 
 setwd("C:\\Users\\letic\\OneDrive\\Área de Trabalho\\PESQUISA\\mini artigo\\getHours")
@@ -6,20 +5,12 @@ df_activity <- read.table("commitHours.data", header = TRUE, sep=",")
 
 df_activity
 
-#pl <- ggplot(df_activity,aes(x = Hora, y=quantidade)) +
-#scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-#scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-#geom_bar(stat = "identity", width=0.2)+
+pl <- ggplot(df_activity,aes(x = Hora, y=Frequencia)) +
+  ggtitle("Commits em função da hora do dia")+
+  
+  theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
+  geom_line(lwd=1.5)+
+  scale_y_continuous(limits = c(0, 500)) +
+  scale_x_continuous(breaks=(0:23))
 
-#theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
-#geom_line(color =" blue")
-
-plot(df_activity, xaxt="none", main="Quantidade de commits em função da hora do dia")
-axis(1, seq(0,25,1), font=2)
-axis(2, seq(0,500,50), font=2)
-geom_line(color =" blue")
-
-
-
-
-#print(pl)
+print(pl)
