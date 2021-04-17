ffb09a686cf6f14bdcabae2e932b96a51c13fa7e
leticiafraga <74565664+leticiafraga@users.noreply.github.com>
2021-04-17 15:07:20 -0300

    Update getHours.py

--- getHours/getHours.py
+++ getHours/getHours.py
@@ -12,42 +12,31 @@ def countHours(fileActivity, hourMap):
 
         fileActivity.write(str(i)+","+str(hourMap[i])+"\n")
         
-              
+        
+                
 def checkHours(dados_api, hourMap, fileLogRaw, fileActivity):
     
     hourMap = [0]*24
 
     for i in dados_api:
         
-        #author = i['commit']['author']['email']
-        #print(author)
         if type(i)==dict:
-            commit =  i['commit']
-            author = commit.get('author')
-            date = author.get('date')
-
-
+            
+            date = i['commit']['author']['date']
             day = date.split("T")[0]
             year = day.split("-")[0]
             #print(year)
             if year =="2021":
                 
-                #"2021-04-14T17:55:56Z"
-                
-                
+                #Formato: "2021-04-14T17:55:56Z"
+               
                 time = date.split("T")[1]
                 hour = time.split(":")[0]
                 
                 hourMap[int(hour)] += 1
-                
-                print(hour +","+ str(hourMap[int(hour)])+"\n")
 
                 fileLogRaw.write(hour +","+ str(hourMap[int(hour)])+"\n")
-    soma =0
-    for i in hourMap:
-        soma += i
-    #print (soma)            
-            
+    
     countHours(fileActivity, hourMap)
             
         
@@ -56,10 +45,7 @@ dados_api = json.load(fileJSON)
 fileLogRaw = open("logBasicRawCH.data", "w")
 hourMap = []
 arqHours = open("commitHours.data", "w")
-arqHours.write("Hora, quantidade\n")
-
+arqHours.write("Hora, Frequencia\n")
 checkHours(dados_api, hourMap,fileLogRaw, arqHours)
 fileLogRaw.close()
 arqHours.close()
-
-
