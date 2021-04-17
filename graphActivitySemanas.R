1dff69eda843e836cb2b161ce057c17f25801cd8
leticiafraga <74565664+leticiafraga@users.noreply.github.com>
2021-04-17 14:54:56 -0300

    Update graphActivitySemanas.R

--- graphActivitySemanas.R
+++ graphActivitySemanas.R
@@ -1,22 +1,22 @@
-
 library(ggplot2)
 
 setwd("C:\\Users\\letic\\OneDrive\\Área de Trabalho\\PESQUISA\\mini artigo")
 df_activity <- read.table("activityWD.data", header = TRUE, sep=",")
+Dias<-c("Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado","Domingo")
 
 df_activity
 
-#pl <- ggplot(df_activity,aes(x = Dia, y=Frequência)) +
-#scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-#scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-#geom_bar(stat = "identity", width=0.2)+
+pl <- ggplot(df_activity,aes(x = Dia, y=Frequencia)) +
+ggtitle("Commits por dia da semana")+
 
-#theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
-#geom_line(color =" blue")
-
-plot(df_activity, xaxt="none", main="Quantidade de commits em função do dia da semana")
-axis(1, seq(0,25,1), font=2)
-axis(2, seq(0,1200,200), font=2)
+theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Quantidade (commits)") +
+geom_line(color ="blue", lwd=1.5)+
+scale_x_continuous(breaks=(0:6),labels= c("S", "T", "Q", "Q", "S", "S", "D"))
 
 
-#print(pl)
+print(pl)
+
+#plot(df_activity, xaxt="n",main="Quantidade de commits em função do dia da semana", type="l")
+#axis(1, at=0:6, labels=Dias, font=2)
+#axis(2, seq(0,1200,200), font=2)
+
